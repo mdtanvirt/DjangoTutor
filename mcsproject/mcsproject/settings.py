f8f60ec10afd725710613af9f4797ee4f86000ce
Tanvir Rahman <trahman@paradynamix.com>
2021-03-28 23:50:40 +0600

    DB configuration and other related confuguration

--- mcsproject/mcsproject/settings.py
+++ mcsproject/mcsproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mcsapp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mcsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freedbtech_mysite',
+        'USER': 'freedbtech_mdtanvirt',
+        'PASSWORD': 'Tanvir24',
+        'HOST': 'freedb.tech',
+        'PORT': '3306',
     }
 }
 
